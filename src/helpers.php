885e6986031bd7eb0ea725473302453b41af138c
James Austen <aus.james@gmail.com>
2020-09-06 16:51:04 +0100

    Initial commit

---
+++ src/helpers.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JamesAusten;
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+/**
+ * PHP-CS-Fixer configuration provider
+ *
+ * @see https://laravel-news.com/sharing-php-cs-fixer-rules-across-projects-and-teams
+ *
+ * @param \PhpCsFixer\Finder $finder
+ * @param array              $rules
+ * @param bool               $cache
+ *
+ * @return \PhpCsFixer\Config
+ */
+function styles(Finder $finder, array $rules = [], bool $cache = true): Config
+{
+    $rules = array_merge(require __DIR__ . '/rules.php', $rules);
+
+    return Config::create()
+        ->setFinder($finder)
+        ->setRiskyAllowed(true)
+        ->setRules($rules)
+        ->setUsingCache($cache);
+}
